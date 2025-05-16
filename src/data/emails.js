4400c03382e18c526737ebc76dd83a722944622e
Sparsha Srinath <sparshasrinath92@gmail.com>
2025-05-16 16:06:19 -0700

    Updates

--- src/data/emails.js
+++
@@ -1,6 +0,0 @@
-// /data/emails.js
-export const emails = [
-    { id: 1, sender: "You", subject: "Happy Birthday!", date: "2023-05-15", content: "🎉 Wishing you an amazing year ahead! Here's to more adventures together." },
-    { id: 2, sender: "You", subject: "Anniversary Love", date: "2024-02-14", content: "Every day with you is the best commit in my life 💻❤️" },
-  ];
-  
