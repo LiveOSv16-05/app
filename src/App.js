1b9a8c722f6d2c8d97cd4c7f733b18be187d133b
Sparsha Srinath <sparshasrinath92@gmail.com>
2025-05-16 14:07:03 -0700

    Added buid file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 
 import Desktop from './components/Desktop';
-import SlideshowWindow from './components/SlideshowWindow';
 export default function App() {
   return <Desktop />;
   // return <SlideshowWindow />;
