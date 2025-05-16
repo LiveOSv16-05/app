58bb3b4d83fecb8390e5d2573bd9e0597178e719
Sparsha Srinath <sparshasrinath92@gmail.com>
2025-05-16 14:01:34 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+
+import Desktop from './components/Desktop';
+import SlideshowWindow from './components/SlideshowWindow';
+export default function App() {
+  return <Desktop />;
+  // return <SlideshowWindow />;
+
+}
